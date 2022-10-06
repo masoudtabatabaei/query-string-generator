50dd8fb270b0312bf7377c4ed24c191e962aaf40
masoudtabatabaei <masoudtabatabaei20@gmail.com>
2022-10-06 07:41:57 +0330

    Add UrlGenerator file

---
+++ src/urlGenerator.js
@@ -0,0 +1,25 @@
+export const InitialState = {
+    query: "",
+    passQuery: false,
+    page: 1,
+    size: 10
+};
+
+export const UPDATE_QUERY = {type: "UPDATE_QUERY", data:{ query:InitialState.query }};
+export const UPDATE_PAGE = {type: "UPDATE_PAGE", data:{ pageNo:InitialState.page }};
+export const PASS_QUERY = {type: "PASS_QUERY", data:{ passQuery:InitialState.passQuery }};
+
+export const reducer = (state, action) => {
+    switch(action.type) {
+        case "UPDATE_QUERY":
+            console.log("Query Updated ===");
+            return {...state , query: action.data.query};
+            case "UPDATE_PAGE":
+                return {...state , page: action.data.pageNo};
+            case "PASS_QUERY":
+            console.log("Query Passed ===");
+            return {...state , passQuery: action.data.passQuery};
+        default:
+            return state;
+    }
+}
